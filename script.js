7f9ff8cb9df4034ae3b85683afabc60e9a94083a
Unknown <trbovic.alexandre@hotmail.fr>
2018-01-15 15:57:05 +0100

    Formulaire contact

---
+++ script.js
@@ -0,0 +1,7 @@
+window.alert("OK");
+
+void function confirm()
+{
+    var adresseMail = document.getElementById("e-mail");
+    alert("Confirmez votre adresse e-mail "+adresseMail.value+" et l'envoie du formulaire");
+};
